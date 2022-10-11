2e48618df022baeef8bf8d20f14d65febf66fc34
BadReese <cjyjia@live.cn>
2022-10-11 15:12:46 +0800

    fix iOS build error

--- ios/Classes/FlutterTtsPlugin.m
+++ ios/Classes/FlutterTtsPlugin.m
@@ -1,5 +1,9 @@
 #import "FlutterTtsPlugin.h"
+#if __has_include(<flutter_tts/flutter_tts-Swift.h>)
 #import <flutter_tts/flutter_tts-Swift.h>
+#else
+#import "flutter_tts-Swift.h"
+#endif
 
 @implementation FlutterTtsPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
